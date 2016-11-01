96c407a3300f45a2e3a64a51a4b21b1e99552663
Prajay Shakya <prajayshakya@gmail.com>
2016-11-01 17:01:03 -0500

    shuffled,and distributted cards in service layer

--- js/controller/cardCtrl.js
+++ js/controller/cardCtrl.js
@@ -1,25 +1,25 @@
 angular.module("callBreak").controller("cardCtrl",["$scope", "cardService", function($scope, cardService){
     $scope.cards = [];
+    //$scope.shufflecards = [];
     
     
-    
-    $scope.$watch(function(){
-        return cardService.cards;
-    },
-        function(newval, oldVal){
-        if (oldVal !=  newval)
-        {
-            console.log("from watchlist");
-            console.log("oldVal");     
-            console.log(oldVal);
-            console.log("New val");
-            console.log(newval);
-            
-                
-            $scope.cards=newval;
-                console.log("from controller");
-                console.log(cardService.cards);
-        }
-    });
-    
+//    $scope.$watch(function(){
+//        return cardService.cards;
+//    },
+//        function(newval, oldVal){
+//        if (oldVal !=  newval)
+//        {
+//            console.log("from watchlist");
+//            console.log("oldVal");     
+//            console.log(oldVal);
+//            console.log("New val");
+//            console.log(newval);
+//            
+//                
+//            $scope.cards=newval;
+//                console.log("from controller");
+//                console.log(cardService.cards);
+//        }
+//    });
+//    
 }]);
--- js/service/callbreak_services.js
+++ js/service/callbreak_services.js
@@ -2,9 +2,55 @@ angular.module("callBreak").service('cardService',['$http', function($http){
     var obj = this;
     $http.get("/service/callBreak").then(function(response){
         
+        //cards collected from database
         obj.cards = response.data;
-        
-        console.log("from service");
+        console.log("from service, cards");
         console.log(obj.cards);
-    })
-}])
+        
+        //cards shuffled 
+        obj.shufflecards = [];
+        console.log(obj.shufflecards);
+        while(obj.shufflecards.length != obj.cards.length){
+            var random = (parseInt(Math.random()*obj.cards.length));
+            //console.log(random);
+            if(obj.shufflecards.indexOf(obj.cards[random]) == -1){
+                obj.shufflecards.push(obj.cards[random]);
+                
+            }
+        }
+        console.log("from service, shuffledCards")
+        console.log(obj.shufflecards);
+        
+        //cards distributted into 4 players 
+        
+        obj.player1=[];
+        obj.player2=[];
+        obj.player3=[];
+        obj.player4=[];
+        
+        for(var i = 0; i<obj.shufflecards.length; i++){
+            switch(i%4){
+                case 0:
+                    obj.player1.push(obj.shufflecards[i]);
+                    break;
+                case 1:
+                    obj.player2.push(obj.shufflecards[i]);
+                     break;
+                case 2:
+                    obj.player3.push(obj.shufflecards[i]);
+                     break;
+                case 3:
+                    obj.player4.push(obj.shufflecards[i]);
+                     break;
+            }
+        }
+        
+        console.log("Cards distribbuted into 4 players");
+        console.log(obj.player1);
+        console.log(obj.player2);
+        console.log(obj.player3);
+        console.log(obj.player4);
+        
+    });
+    
+}]);
